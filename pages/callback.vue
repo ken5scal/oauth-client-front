6c4068a9b87337e8554c6d2d6c96fbff65805e99
Kengo Suzuki <kengoscal@gmail.com>
2019-05-22 03:16:02 +0900

    added server side

---
+++ pages/callback.vue
@@ -0,0 +1,23 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {},
+            error: null,
+        }
+    },
+    mounted() {
+        axios.get('http://localhost:3000/server/authorize/callback', {
+            params: this.$route.query,
+        }).then(res => {
+            this.user = res.data.user;
+        }).catch(err => {
+            this.error = err;
+        })
+    }
+}
+</script>
